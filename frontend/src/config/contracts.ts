fbc646454785d7f0468452d1ec1878189aab6085
elliotzone <zskgvik2470@outlook.com>
2025-10-24 15:42:31 +0800

    deploy testnet

--- frontend/src/config/contracts.ts
+++ frontend/src/config/contracts.ts
@@ -1,7 +1,7 @@
 import { sepolia } from 'wagmi/chains';
 import { SECRET_CHAT_ABI } from './secretChatAbi';
 
-const envAddress = import.meta.env.VITE_SECRETCHAT_ADDRESS;
+const envAddress = "0x1F1B4D5D42caFc496E81DBfbbF7285075be3a8FF";
 
 export const SECRET_CHAT_ADDRESS = envAddress ? (envAddress as `0x${string}`) : undefined;
 export const SECRET_CHAT_CHAIN = sepolia;
